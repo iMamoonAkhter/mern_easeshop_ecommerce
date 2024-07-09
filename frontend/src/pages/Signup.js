4f13646d30345ebfb2ba03ea50d07c16a3406d9b
Mamoon Akhter <145218680+iMamoonAkhter@users.noreply.github.com>
2024-07-09 17:39:05 +0500

    Update Signup.js

--- frontend/src/pages/Signup.js
+++ frontend/src/pages/Signup.js
@@ -26,7 +26,7 @@ function Signup() {
       return handleError("name, email and password are required");
     }
     try {
-      const url = `https://easeshopecommerce.vercel.app/signup`;
+      const url = `https://easeshopecommerce.vercel.app/register`;
       const response = await fetch(url, {
         method: "POST",
         headers: {
