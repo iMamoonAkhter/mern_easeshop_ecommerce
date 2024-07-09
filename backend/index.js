f199fbad12182ff536a4ab8c7c85f72264504a21
Mamoon Akhter <145218680+iMamoonAkhter@users.noreply.github.com>
2024-07-09 17:27:55 +0500

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -14,7 +14,11 @@ app.get('/ping', (req, res) => {
 });
 
 app.use(bodyParser.json());
-app.use(cors());
+app.use(cors({
+    origin: ["https://easeshopecommerce.vercel.app/"],
+    methods: ["POST", "GET"],
+    credentials: true
+}));
 app.use('/auth', AuthRouter);
 app.use('/products', ProductRouter);
 
